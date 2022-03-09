53a132efcf292a3f0466aae9fceb9fc150b8d0ab
nagy-oliver <oliver.nagy510@gmail.com>
2022-03-09 21:02:39 +0100

    board class

--- src/main/java/com/compsci/App.java
+++ src/main/java/com/compsci/App.java
@@ -1,13 +1,32 @@
 package com.compsci;
+import io.socket.*;
+import java.io.*;
+import org.json.*;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Board test1 = new Board();
     }
+}
+
+class Board {
+    public File myShips;
+    public File opponentShips;
+    public Board() {
+        try {
+            myShips = new File("myships.game");
+            opponentShips = new File("target/opponentships.game");
+            if(opponentShips.createNewFile()) {
+                System.out.println("Created new file: " + opponentShips.getName());
+            } else {
+                System.out.println("the file already exists");
+            }
+        } catch (IOException e) {
+            System.out.println("an error occured");
+            e.printStackTrace();
+        } 
+    }
+
 }
