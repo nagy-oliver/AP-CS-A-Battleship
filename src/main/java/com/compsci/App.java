ae1cfe7d455bc25e60ac6dde64ad27d8401c84be
nagy-oliver <oliver.nagy510@gmail.com>
2022-03-10 22:29:17 +0100

    Board separate file

--- src/main/java/com/compsci/App.java
+++ src/main/java/com/compsci/App.java
@@ -52,33 +52,3 @@ public class App
         input.close();
     }
 }
-
-class Board {
-    public File myShipsFile;
-    public int[][] myShips;
-    //public File opponentShips;
-    public Board(int[] boardSize) {
-        //define myShips size:
-        myShips = new int[boardSize[1]][boardSize[0]];
-        //create a 2d array from the .game file
-        try {
-            myShipsFile = new File("target/myships.game");
-            Scanner fileReader = new Scanner(myShipsFile);
-            int line = 0;
-            while(fileReader.hasNextLine()) {
-                String newLine = fileReader.nextLine();
-                if(newLine.length() != boardSize[0]) {
-                    System.out.println("Invalid game file");
-                    System.exit(1);
-                }
-                for(int i = 0; i < newLine.length(); i++) {
-                    myShips[line][i] = Integer.parseInt(Character.toString(newLine.charAt(i)));
-                }
-                line++;
-            }
-        } catch (Exception e) { //most likely in case of invalid dimensions of the file
-            System.out.println("Invalid game file");
-            System.exit(1);
-        }
-    }
-}
