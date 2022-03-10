baa05a1514fc45a46aab72254174e80b1047f266
nagy-oliver <oliver.nagy510@gmail.com>
2022-03-10 22:24:29 +0100

    config in new file

--- src/main/java/com/compsci/App.java
+++ src/main/java/com/compsci/App.java
@@ -2,8 +2,6 @@ package com.compsci;
 import com.compsci.Coms.*;
 import java.util.*;
 import java.io.*;
-import org.json.simple.*;
-import org.json.simple.parser.*;
 
 public class App 
 { 
@@ -82,52 +80,5 @@ class Board {
             System.out.println("Invalid game file");
             System.exit(1);
         }
-    }
-}
-
-class Config {
-    public int[] size = new int[2]; //[width, height]
-    public int ships;
-    public int[] shipSizes;
-
-    public Config() {
-        //reads own JSON and assigns instance variables
-        JSONParser parser = new JSONParser();
-
-        try {
-            Object obj = parser.parse(new FileReader("target/config.json"));
-
-            JSONObject jsonObject =  (JSONObject) obj;
-
-            JSONArray arr = (JSONArray) jsonObject.get("size");
-            for(int i = 0; i<arr.toArray().length; i++) {
-                size[i] = ((Long) arr.toArray()[i]).intValue();
-            }
-
-            ships = ((Long) jsonObject.get("ships")).intValue();
-            shipSizes = new int[ships];
-
-            arr = (JSONArray) jsonObject.get("shipSizes");
-            for(int i = 0; i<arr.toArray().length; i++) {
-                shipSizes[i] = ((Long) arr.toArray()[i]).intValue();
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-    
-    /*public boolean validate(Board myBoard) {
-        //checks whether board is corresponding to the config, otherwise exits
-        //check size of the board:
-
-        //check number of ships:
-
-        //check the size of ships:
-    }*/
-
-    public boolean compareConfigs(Config clientConfig) {
-        return clientConfig.equals(this);
     }
 }
